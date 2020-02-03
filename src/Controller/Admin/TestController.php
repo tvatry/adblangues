942b5554ac108797f5492582d6406cb8fe007962
Tony Vatry <tvatry@gmail.com>
2020-02-03 09:39:33 +0100

    Add edit questions function

--- src/Controller/Admin/TestController.php
+++ src/Controller/Admin/TestController.php
@@ -143,6 +143,53 @@ class TestController extends AbstractController
     }
 
     /**
+     * @Route("/admin/questionnaire/questions/edition/{id}", name="admin_test_edit_questions")
+     * @param $id
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     */
+
+    public function editQuestions($id, Request $request)
+    {
+        $entitymanager = $this->getDoctrine()->getManager();
+        $question = new Question();
+        $answer = new Answer();
+        $test = $entitymanager->getRepository(Test::class)->find($id);
+        $level = $entitymanager->getRepository(Level::class)->find('22');
+        $form = $this->createForm(BackQuestionsType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+            $length = count($form->get('questions')->getData());
+
+            for ($i=1; $i<$length+1; $i++){
+                $question = new Question();
+                $question->setWording($form->get('questions')[$i]['wording']->getData());
+                $question->setLevel($level);
+                $question->setTest($test);
+                $entitymanager->persist($question);
+                $entitymanager->flush();
+                for ($j=1; $j<4+1; $j++){
+                    if ($form->get('questions')[$i]['answer'.$j.'']->getData() != null){
+                        $answer = new Answer();
+                        $answer->setAnswer($form->get('questions')[$i]['answer'.$j.'']->getData());
+                        //$answer->setIsConnected($form->get('answers')[$i]['isConnected']->getData());
+                        $answer->setIsConnected($form->get('questions')[$i]['isConnected'.$j.'']->getData());
+                        $answer->setQuestion($entitymanager->getRepository(Question::class)->find($question->getId()));
+                        $entitymanager->persist($answer);
+                        $entitymanager->flush();
+                    }
+                }
+            }
+            return $this->redirectToRoute('admin_test_index');
+        }
+        return $this->render('backoffice/test/add_questions.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
      * @Route("/admin/questionnaire/suppression/{id}", name="admin_test_delete")
      * @param $id
      * @return \Symfony\Component\HttpFoundation\RedirectResponse
