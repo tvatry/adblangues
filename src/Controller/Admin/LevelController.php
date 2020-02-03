0895da37ab6395dc6654ce0c43ee9e7b5d6e2f7f
Tony Vatry <tvatry@gmail.com>
2020-02-03 11:42:50 +0100

    Remove - level edit feature

--- src/Controller/Admin/LevelController.php
+++ src/Controller/Admin/LevelController.php
@@ -23,26 +23,65 @@ class LevelController extends AbstractController
         ]);
     }
 
-    /**
-     * @Route("/admin/niveau/edition/{id}", name="admin_level_update")
-     */
-    public function update($id, Request $request)
-    {
-      $allLevel = $this->getDoctrine()->getManager();
-      $level = $allLevel->getRepository(Level::class)->find($id);
-
-      $form = $this->createForm(LevelType::class, $level);
-      $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $allLevel->flush();
-
-            return $this->redirectToRoute('admin_level_index');
-        }
-
-        return $this->render('backoffice/level/edit.html.twig', [
-            'form' => $form->createView()
-        ]);
-    }
+//    /**
+//     * @Route("/admin/niveau/ajout", name="admin_level_create")
+//     */
+//    public function create(Request $request)
+//    {
+//        $level = new Level();
+//
+//        $form = $this->createForm(LevelType::class);
+//        $form->handleRequest($request);
+//
+//        if ($form->isSubmitted() && $form->isValid()) {
+//            $level = $form->getData();
+//
+//            $entitymanager = $this->getDoctrine()->getManager();
+//            $entitymanager->persist($level);
+//            $entitymanager->flush();
+//
+//            return $this->redirectToRoute('admin_level_index');
+//        }
+//
+//        return $this->render('backoffice/level/create.html.twig', [
+//            'form' => $form->createView(),
+//        ]);
+//    }
+//
+//    /**
+//     * @Route("/admin/niveau/edition/{id}", name="admin_level_update")
+//     */
+//    public function update($id, Request $request)
+//    {
+//      $allLevel = $this->getDoctrine()->getManager();
+//      $level = $allLevel->getRepository(Level::class)->find($id);
+//
+//      $form = $this->createForm(LevelType::class, $level);
+//      $form->handleRequest($request);
+//
+//        if ($form->isSubmitted() && $form->isValid()) {
+//            $allLevel->flush();
+//
+//            return $this->redirectToRoute('admin_level_index');
+//        }
+//
+//        return $this->render('backoffice/level/edit.html.twig', [
+//            'form' => $form->createView()
+//        ]);
+//    }
+//
+//    /**
+//     * @Route("/admin/niveau/suppression/{id}", name="admin_level_delete")
+//     */
+//    public function delete($id, Request $request)
+//    {
+//       $allLevel = $this->getDoctrine()->getManager();
+//       $deleteLevel = $allLevel->getRepository(Level::class)->find($id);
+//
+//       $allLevel->remove($deleteLevel);
+//       $allLevel->flush();
+//
+//      return $this->redirectToRoute('admin_level_index');
+//    }
 
     }
