ffd7ae662e514aa920283d762a1072e9da0e384e
Tony Vatry <tvatry@gmail.com>
2020-02-04 22:52:45 +0100

    Update fixtures

--- src/DataFixtures/LocationsFixtures.php
+++ src/DataFixtures/LocationsFixtures.php
@@ -13,38 +13,116 @@ class LocationsFixtures extends Fixture
     {
       $locations = new Locations();
       $locations->setName('Compiègne');
+        $contact = new Contact();
+        $contact->setName('CASSAN');
+        $contact->setMail('mf.cassan@promeo-formation.fr');
+        $contact->setFirstName('Marie-Françoise');
+        $contact->setLocation($locations);
+        $manager->persist($contact);
+        $contact = new Contact();
+        $contact->setName('MAXIMY');
+        $contact->setMail('i.maximy@promeo-formation.fr');
+        $contact->setFirstName('Isabelle');
+        $contact->setLocation($locations);
+        $manager->persist($contact);
       $manager->persist($locations);
 
       $locations = new Locations();
       $locations->setName('Soissons');
+        $contact = new Contact();
+        $contact->setName('QUEMART');
+        $contact->setMail('l.quemart@promeo-formation.fr');
+        $contact->setFirstName('Laetitia');
+        $contact->setLocation($locations);
+        $manager->persist($contact);
       $manager->persist($locations);
 
       $locations = new Locations();
       $locations->setName('Beauvais');
+        $contact = new Contact();
+        $contact->setName('BLIN');
+        $contact->setMail('z.blin@promeo-formation.fr');
+        $contact->setFirstName('Zorah');
+        $contact->setLocation($locations);
+        $manager->persist($contact);
       $manager->persist($locations);
 
         $locations = new Locations();
         $locations->setName('Amiens');
+        $contact = new Contact();
+        $contact->setName('SYMIEN');
+        $contact->setMail('c.symien@promeo-formation.fr');
+        $contact->setFirstName('Catherine');
+        $contact->setLocation($locations);
+        $manager->persist($contact);
+        $contact = new Contact();
+        $contact->setName('CAILLY');
+        $contact->setMail('a.cailly@promeo-formation.fr');
+        $contact->setFirstName('Alexandra');
+        $contact->setLocation($locations);
+        $manager->persist($contact);
         $manager->persist($locations);
 
         $locations = new Locations();
         $locations->setName('Friville');
+        $contact = new Contact();
+        $contact->setName('POIRET');
+        $contact->setMail('f.poiret@promeo-formation.fr');
+        $contact->setFirstName('Fanny');
+        $contact->setLocation($locations);
+        $manager->persist($contact);
         $manager->persist($locations);
 
         $locations = new Locations();
         $locations->setName('Saint-Quentin');
+        $contact = new Contact();
+        $contact->setName('HEYDT-BLANQUART');
+        $contact->setMail('l.heydtblanquart@promeo-formation.fr');
+        $contact->setFirstName('Laurence');
+        $contact->setLocation($locations);
+        $manager->persist($contact);
+
+        $contact = new Contact();
+        $contact->setName('CERISIER');
+        $contact->setMail('l.cerisier@promeo-formation.fr');
+        $contact->setFirstName('L');
+        $contact->setLocation($locations);
+        $manager->persist($contact);
+
+        $contact = new Contact();
+        $contact->setName('GUEDES');
+        $contact->setMail('a.guedes@promeo-formation.fr');
+        $contact->setFirstName('A');
+        $contact->setLocation($locations);
+        $manager->persist($contact);
         $manager->persist($locations);
 
       $locations = new Locations();
       $locations->setName('Senlis');
 
       $contact = new Contact();
-      $contact->setName('Doe');
-      $contact->setMail('johndoe@mail.com');
-      $contact->setFirstName('John');
+      $contact->setName('PAIN');
+      $contact->setMail('s.pain@promeo-formation.fr');
+      $contact->setFirstName('Susan');
       $contact->setLocation($locations);
-      $manager->persist($locations);
       $manager->persist($contact);
+
+      $contact = new Contact();
+      $contact->setName('DUMALANEDE');
+      $contact->setMail('c.dumalanede@promeo-formation.fr');
+      $contact->setFirstName('Cécile');
+      $contact->setLocation($locations);
+      $manager->persist($contact);
+
+        $contact = new Contact();
+        $contact->setName('QUEMART');
+        $contact->setMail('l.quemart@promeo-formation.fr');
+        $contact->setFirstName('Laetitia');
+        $contact->setLocation($locations);
+        $manager->persist($contact);
+
+      $manager->persist($locations);
+
       $manager->flush();
 
     }
--- src/DataFixtures/SuperAdminFixtures.php
+++ src/DataFixtures/SuperAdminFixtures.php
@@ -33,6 +33,115 @@ class SuperAdminFixtures extends Fixture
         $user->setIsActive(1);
         $user->setRoles(array("ROLE_SUPER_ADMIN"));
         $manager->persist($user);
+
+        $user = new User();
+        $user->setUsername('c.symien');
+        $user->setPassword($this->encoder->encodePassword($user, 'promeo'));
+        $user->setEmail('c.symien@promeo-formation.fr');
+        $user->setToken(time());
+        $user->setIsActive(1);
+        $user->setRoles(array("ROLE_FORMATEUR"));
+        $manager->persist($user);
+
+        $user = new User();
+        $user->setUsername('a.cailly');
+        $user->setPassword($this->encoder->encodePassword($user, 'promeo'));
+        $user->setEmail('a.cailly@promeo-formation.fr');
+        $user->setToken(time());
+        $user->setIsActive(1);
+        $user->setRoles(array("ROLE_FORMATEUR"));
+        $manager->persist($user);
+
+        $user = new User();
+        $user->setUsername('z.blin');
+        $user->setPassword($this->encoder->encodePassword($user, 'promeo'));
+        $user->setEmail('z.blin@promeo-formation.fr');
+        $user->setToken(time());
+        $user->setIsActive(1);
+        $user->setRoles(array("ROLE_FORMATEUR"));
+        $manager->persist($user);
+
+        $user = new User();
+        $user->setUsername('mf.cassan');
+        $user->setPassword($this->encoder->encodePassword($user, 'promeo'));
+        $user->setEmail('mf.cassan@promeo-formation.fr');
+        $user->setToken(time());
+        $user->setIsActive(1);
+        $user->setRoles(array("ROLE_FORMATEUR"));
+        $manager->persist($user);
+
+        $user = new User();
+        $user->setUsername('i.maximy');
+        $user->setPassword($this->encoder->encodePassword($user, 'promeo'));
+        $user->setEmail('i.maximy@promeo-formation.fr');
+        $user->setToken(time());
+        $user->setIsActive(1);
+        $user->setRoles(array("ROLE_FORMATEUR"));
+        $manager->persist($user);
+
+        $user = new User();
+        $user->setUsername('f.poiret');
+        $user->setPassword($this->encoder->encodePassword($user, 'promeo'));
+        $user->setEmail('f.poiret@promeo-formation.fr');
+        $user->setToken(time());
+        $user->setIsActive(1);
+        $user->setRoles(array("ROLE_FORMATEUR"));
+        $manager->persist($user);
+
+        $user = new User();
+        $user->setUsername('l.heydtblanquart');
+        $user->setPassword($this->encoder->encodePassword($user, 'promeo'));
+        $user->setEmail('l.heydtblanquart@promeo-formation.fr');
+        $user->setToken(time());
+        $user->setIsActive(1);
+        $user->setRoles(array("ROLE_FORMATEUR"));
+        $manager->persist($user);
+
+        $user = new User();
+        $user->setUsername('l.cerisier');
+        $user->setPassword($this->encoder->encodePassword($user, 'promeo'));
+        $user->setEmail('l.cerisier@promeo-formation.fr');
+        $user->setToken(time());
+        $user->setIsActive(1);
+        $user->setRoles(array("ROLE_FORMATEUR"));
+        $manager->persist($user);
+
+        $user = new User();
+        $user->setUsername('a.guedes');
+        $user->setPassword($this->encoder->encodePassword($user, 'promeo'));
+        $user->setEmail('a.guedes@promeo-formation.fr');
+        $user->setToken(time());
+        $user->setIsActive(1);
+        $user->setRoles(array("ROLE_FORMATEUR"));
+        $manager->persist($user);
+
+        $user = new User();
+        $user->setUsername('s.pain');
+        $user->setPassword($this->encoder->encodePassword($user, 'promeo'));
+        $user->setEmail('s.pain@promeo-formation.fr');
+        $user->setToken(time());
+        $user->setIsActive(1);
+        $user->setRoles(array("ROLE_FORMATEUR"));
+        $manager->persist($user);
+
+        $user = new User();
+        $user->setUsername('c.dumalanede');
+        $user->setPassword($this->encoder->encodePassword($user, 'promeo'));
+        $user->setEmail('c.dumalanede@promeo-formation.fr');
+        $user->setToken(time());
+        $user->setIsActive(1);
+        $user->setRoles(array("ROLE_FORMATEUR"));
+        $manager->persist($user);
+
+        $user = new User();
+        $user->setUsername('l.quemart');
+        $user->setPassword($this->encoder->encodePassword($user, 'promeo'));
+        $user->setEmail('l.quemart@promeo-formation.fr');
+        $user->setToken(time());
+        $user->setIsActive(1);
+        $user->setRoles(array("ROLE_FORMATEUR"));
+        $manager->persist($user);
+
         $manager->flush();
     }
 }
